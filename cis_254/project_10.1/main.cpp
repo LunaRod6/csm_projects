4ebad30842ec968b6957250a220df578302dc1b2
Rodrigo Nochez <00378021@uca.edu.sv>
2025-03-31 22:10:30 -0700

    Add printReport function taking the results of the good answers to generate the reports.

--- cis_254/project_10.1/main.cpp
+++ cis_254/project_10.1/main.cpp
@@ -18,39 +18,45 @@
 using namespace std;
 
 void getProbsPerSet(int& a);
-void generateOperands(int& a , int& b);
+void generateOperands(int& a , int& b, int mNum);
 void calculateCorrectAnswer(int a , int b , int& c , char symbol );
-void checkAnswer(int total , int user);
+void checkAnswer(int total , int user, int& g);
 void doOneProblem(char symbol ,int a , int b , int& d);
-void doOneSet(char symbol, int n);
+void printHeader(char c);
+void getMaxNum(int& m);
+void doOneSet(char symbol, double n, double& g);
+void printReport(double g1, double g2, double g3, double mN);
 
 
 int main(){
 
-    int n;
+    double n;
+    double good1;
+    double good2;
+    double good3;
     
     getProbsPerSet(n);
     //srand(static_cast<unsigned>(time(nullptr)));
-    doOneSet('+', n);
-    doOneSet('-', n);
-    doOneSet('*', n);
+    doOneSet('+', n, good1);
+    doOneSet('-', n, good2);
+    doOneSet('*', n, good3);
 
     return 0;
 }
 
 //Ask the user how many sets they want to operate.
-void getProbsPerSet(int& a){
+void getProbsPerSet(double& a){
 
     cout << "Enter problems per set: ";
     cin >> a;
 
 }
 
-void generateOperands(int& a , int& b){
+void generateOperands(int& a , int& b, int mNum){
 
-    //Generates a number between 0 - 100 inclusive
-     a = rand() % 101;
-     b = rand() % 101;
+    //Generates a number between 0 and the mNum chosen by the user inclusive
+     a = rand() % (mNum + 1);
+     b = rand() % (mNum + 1);
 
 }
 
@@ -73,11 +79,12 @@ void calculateCorrectAnswer(int a , int b , int& c , char symbol){
 }
 
 //Evaluates the user's answer with the computer's.
-void checkAnswer(int total , int user){
+void checkAnswer(int total , int user, int& g){
 
     if (total == user) {
 
         cout << "correct" << endl;
+        g++;
 
     } else {
 
@@ -98,16 +105,15 @@ void doOneProblem(char symbol, int a , int b , int& d){
     
 }
 
-void doOneSet(char symbol, int n){
+//Prints what number of set is going to be done.
+void printHeader(char c){
 
-    cout << endl;
-    
-    if (symbol == '+'){
+    if (c == '+'){
 
         cout << "Set #1" << endl;
         cout << "----------" << endl;
 
-    } else if (symbol == '-'){
+    } else if (c == '-'){
 
         cout << "Set #2" << endl;
         cout << "----------" << endl;
@@ -118,6 +124,27 @@ void doOneSet(char symbol, int n){
         cout << "----------" << endl;
 
     }
+
+}
+
+
+void getMaxNum(int& m){
+
+    cout << "What is the maximum number for this set? ";
+    cin >> m;
+
+}
+
+//Does the the amount of set that the user choose to use
+void doOneSet(char symbol, double n, double& g){
+
+    int maxNum;
+
+    cout << endl;
+
+    printHeader(symbol);
+
+    getMaxNum(maxNum);
     
     //Create exercises using the variable set from the user 
     for(int i = 0 ; i < n ; i++){
@@ -126,12 +153,22 @@ void doOneSet(char symbol, int n){
         int c;
         int d;
 
-        generateOperands(a , b);
+        generateOperands(a , b, maxNum);
         doOneProblem(symbol , a , b , d);
         calculateCorrectAnswer(a , b , c , symbol);
-        checkAnswer(c , d);
+        checkAnswer(c , d, g);
 
     }
     
+}
+
+//Print Report of the results of the user
+void printReport(double g1, double g2, double g3, double mN){
+
+    cout << "Set#1: You got " << g1 << " correct out of "<< mN << " for " <<  fixed << setprecision(1) << (g1 * 100) / mN << "%" << endl;
+    cout << "Set#2: You got " << g2 << " correct out of "<< mN << " for " <<  fixed << setprecision(1) << (g2 * 100) / mN << "%" << endl;
+    cout << "Set#1: You got " << g1 << " correct out of "<< mN << " for " <<  fixed << setprecision(1) << (g3 * 100) / mN << "%" << endl;
+    cout << "Overall you got "<< g1 + g2 + g3 << " correct out of " << mN * 3 << " for " << fixed << setprecision(1) << (mN * 3 * 100) / mN << "%" << endl;
+
 }
 
